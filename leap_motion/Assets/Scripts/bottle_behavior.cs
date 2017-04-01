90247619e835c5a3c003659fda91b3b6dbdc3f00
Sophie Mai Chau <sophiemaichau@Sophies-MacBook-Pro.local>
2017-04-01 17:56:04 +0200

    can now manipulate with the bottles

--- leap_motion/Assets/Scripts/bottle_behavior.cs
+++ leap_motion/Assets/Scripts/bottle_behavior.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bottle_behavior : MonoBehaviour {
+	Rigidbody rb;
+	float velocity;
 
 	// Use this for initialization
-	void Start () {
-        GetComponent<Rigidbody>().position = Vector3.zero;
+	void Start (){
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		rb.angularDrag += 1f;
+		velocity = rb.angularVelocity.magnitude;
+		Debug.Log (velocity);
 	}
 }
