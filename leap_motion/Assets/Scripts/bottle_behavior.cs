a8f8a0c1c2504e28d3903689be2824a6f1afd0ef
Sophie Mai Chau <sophiemaichau@Sophies-MacBook-Pro.local>
2017-04-01 15:08:52 +0200

    added initial scene

---
+++ leap_motion/Assets/Scripts/bottle_behavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bottle_behavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().position = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
