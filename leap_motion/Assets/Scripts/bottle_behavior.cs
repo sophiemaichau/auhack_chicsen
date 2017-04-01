74a39df8f3bf633238d9e2f01d07ecaa6f2790ff
Sophie Mai Chau <sophiemaichau@Sophies-MacBook-Pro.local>
2017-04-02 00:06:07 +0200

    fixed stocking loop

--- leap_motion/Assets/Scripts/bottle_behavior.cs
+++ leap_motion/Assets/Scripts/bottle_behavior.cs
@@ -1,46 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-using System.IO.Ports;
 
 public class bottle_behavior : MonoBehaviour {
 	Rigidbody rb;
 	float rotation = 0;
 	float oldRotation = 0;
 	float rotationDiff = 0;
-	SerialPort port;
+	bool isRotating = false;
+	List<float> List = new List<float>();
+	float degreesMoved;
 
 	// Use this for initialization
 	void Start (){
 		rb = GetComponent<Rigidbody>();
-		port = new SerialPort ("/dev/cu.usbmodem1421", 9600);
-		port.Open();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (port == null) {
-			return;
+		rb.angularDrag += 1f;
+		rotation = rb.transform.eulerAngles.x;
+		rotationDiff = Mathf.Abs (oldRotation - rotation);
+
+		if (rotationDiff > 1 && rotationDiff < 300) {
+			isRotating = true;
+			List.Add (rotationDiff);
+			degreesMoved = SumArray(List.ToArray());
 		}
 
-		if (port.IsOpen) {
-			port.ReadTimeout = 1;
-			rb.angularDrag += 1f;
-			rotation = rb.transform.eulerAngles.x;
-			rotationDiff = Mathf.Abs (oldRotation - rotation);
-
-			if (rotationDiff > 0) {
-				WriteToArduino ("PING");
-				Debug.Log ("Rotating");
-				rotationDiff = 0;
-			}
-			oldRotation = rb.transform.eulerAngles.x;
+		while (rotationDiff == 0 && isRotating == true) {
+			isRotating = false;
+			Debug.Log ("Rotated with: " + degreesMoved);
+			List.Clear ();
+			degreesMoved = 0;
+			rotationDiff = 1;
+			break;
 		}
+
+		oldRotation = rb.transform.eulerAngles.x;
 	}
-		
-	public void WriteToArduino(string message){
-		port.WriteLine(message);
-		port.BaseStream.Flush();
+
+	public float SumArray(float[] toBeSummed)
+	{
+		float sum = 0;
+		foreach (float item in toBeSummed){
+			sum += item;
+		}
+		return sum;
 	}
 }
---
+++ leap_motion/Assets/Scripts/serial_communication.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO.Ports;
+
+public class serial_communication : MonoBehaviour {
+	Rigidbody rb;
+	float rotation = 0;
+	float oldRotation = 0;
+	float rotationDiff = 0;
+	SerialPort port;
+	bool isRotating = false;
+	List<float> List = new List<float>();
+	float max;
+
+	// Use this for initialization
+	void Start (){
+		rb = GetComponent<Rigidbody>();
+		port = new SerialPort ("/dev/cu.usbmodem1421", 9600);
+		port.Open();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (port == null) {
+			return;
+		}
+
+		if (port.IsOpen) {
+			port.ReadTimeout = 1;
+			rb.angularDrag += 1f;
+			rotation = rb.transform.eulerAngles.x;
+			rotationDiff = Mathf.Abs (oldRotation - rotation);
+
+			if (rotationDiff > 0 && rotationDiff < 180) {
+				isRotating = true;
+				List.Add (rotationDiff);
+				max = Mathf.Max (List.ToArray ());
+				rotationDiff = 0;
+
+			} else if (rotationDiff == 0 && isRotating == true) {
+				isRotating = false;
+				WriteToArduino ("1");
+				Debug.Log ("Rotated with: " + max);
+				List.Clear ();
+				max = 0;
+			}
+
+			oldRotation = rb.transform.eulerAngles.x;
+		}
+	}
+
+	public void WriteToArduino(string message){
+		port.WriteLine(message);
+		port.BaseStream.Flush();
+	}
+}
